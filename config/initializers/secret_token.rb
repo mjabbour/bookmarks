4d565ce592f347794a03f716c2bb46429155a6a0
Michael Jabbour <mjabbour@health.nyc.gov>
2014-03-18 17:10:51 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookmarks::Application.config.secret_key_base = '1a9915ea39e28ee729875020f136ecded88393de3e34a7afb9b64d03ea933444851847b954894a891448503460f2500171c5f3e9be668f4c59a6783a4b14baf2'
